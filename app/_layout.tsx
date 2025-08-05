821a743c23647efee21cecf2615672fe98c2cb9f
gw-rodrigues <rodrigues.gw@email.com>
2025-08-05 17:06:54 +0200

    feat: add gluestack components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,7 @@ import 'react-native-reanimated'
 
 import { useColorScheme } from '@/hooks/useColorScheme'
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider'
 import './global.css'
 
 export default function RootLayout() {
@@ -24,12 +25,14 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GluestackUIProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </GluestackUIProvider>
   )
 }
