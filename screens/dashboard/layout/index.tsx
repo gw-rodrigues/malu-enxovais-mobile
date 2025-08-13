f76fd93da3b6e6219aaa69e7e1384aaab8b1b04f
gw-rodrigues <rodrigues.gw@email.com>
2025-08-13 15:46:10 +0200

    update structure

--- screens/dashboard/layout/index.tsx
+++ screens/dashboard/layout/index.tsx
@@ -1,7 +1,9 @@
-import { SafeAreaView } from '@/components/ui/safe-area-view'
-import { ScrollView } from '@/components/ui/scroll-view'
+import { HStack } from '@/components/ui/hstack'
+// import { ScrollView } from '@/components/ui/scroll-view'
 import { VStack } from '@/components/ui/vstack'
-import { useColorScheme } from '@/hooks/useColorScheme'
+
+import { ScrollView } from 'react-native'
+
 import useAuthStore from '@/store/auth.store'
 import { Redirect } from 'expo-router'
 
@@ -10,8 +12,6 @@ type AuthLayoutProps = {
 }
 
 export default function TabLayout(props: AuthLayoutProps) {
-  const colorScheme = useColorScheme()
-
   const { isAuthenticated } = useAuthStore()
 
   if (!isAuthenticated) {
@@ -19,15 +19,15 @@ export default function TabLayout(props: AuthLayoutProps) {
   }
 
   return (
-    <SafeAreaView className="w-full h-full" edges={['bottom']}>
-      <ScrollView
-        className="w-full h-full"
-        contentContainerStyle={{ flexGrow: 1 }}
-      >
-        <VStack space="3xl" className="p-4 mb-20">
+    <ScrollView
+      className="w-full h-full"
+      contentContainerStyle={{ flexGrow: 1 }}
+    >
+      <HStack className="justify-center flex-grow w-full h-full">
+        <VStack className="flex-1 w-full h-full gap-10 px-4 pt-4 pb-28">
           {props.children}
         </VStack>
-      </ScrollView>
-    </SafeAreaView>
+      </HStack>
+    </ScrollView>
   )
 }
